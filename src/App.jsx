9a81e10a7a63f945192b3824f7c4a22ff14306c8
Artem Lysiak <tiomalysiak@gmail.com>
2024-06-17 14:31:08 +0200

    add folder components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
-import ContactForm from "./ContactForm/ContactForm";
-import ContactList from "./ContactList/ContactList";
-import SearchBox from "./SearchBox/SearchBox";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
 import "modern-normalize/modern-normalize.css";
 
 import styles from "./App.module.css";
